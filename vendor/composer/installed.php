682d3d8895c2002e5ef424a2c9874145cb87bc91
ThiagoDzn <thiagocruzdasilva941@gmail.com>
2023-09-20 20:34:22 -0300

    ignorando pasta vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1596c1949eebcdd1363308122827461a030059f6',
+        'name' => 'admin/taskappweb1',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/taskappweb1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1596c1949eebcdd1363308122827461a030059f6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
